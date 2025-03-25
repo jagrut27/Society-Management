bf919aa49f2d8e4bb6980ef1f1e283b4eb19d3b0
pcit109 <pcit109@prudent.local>
2025-03-25 13:12:04 +0530

    Updated .gitignore and project files

--- admin/Backend/Admin/Admin/obj/Debug/net8.0/Admin.AssemblyInfo.cs
+++ admin/Backend/Admin/Admin/obj/Debug/net8.0/Admin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Admin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+046e3cf5078e28d1d7d9c621a50b5efa5c946a60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ad53eeb4bbeaa5720d03ee0a174cf6e1211d5b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Admin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Admin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
